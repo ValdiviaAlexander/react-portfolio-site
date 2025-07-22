3dffddec6ed5f9e7a8893fcbeabb665c103a976d
Alex <brex31102733@gmail.com>
2025-07-22 10:42:17 +0900

    修正

--- src/customHooks/useSkills.js
+++ src/customHooks/useSkills.js
@@ -2,42 +2,54 @@ import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+import { requestStates } from '../constants';
 
 export const useSkills = () => {
-  const [state, dispatch] = useReducer(skillReducer, initialState);
+    const [state, dispatch] = useReducer(skillReducer, initialState);
 
-  useEffect(() => {
-    dispatch({ type: actionTypes.fetch });
-    axios.get('https://api.github.com/users/ValdiviaAlexander/repos')
-      .then((response) => {
-        const languageList = response.data.map(res => res.language)
-        const countedLanguageList = generateLanguageCountObj(languageList)
-        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
-      })
-      .catch(() => {
-        dispatch({ type: actionTypes.error });
-      });
-   }, []);
+    const fetchReposApi = () => {
+        dispatch({ type: actionTypes.fetch });
+        axios.get('https://api.github.com/users/ValdiviaAlexander/repos')
+            .then((response) => {
+            const languageList = response.data.map(res => res.language)
+            const countedLanguageList = generateLanguageCountObj(languageList)
+            dispatch({ type: actionTypes.success, payload: {languageList: countedLanguageList } });
+            })
+            .catch(() => {
+            dispatch({ type: actionTypes.error });
+        });
+    }
 
-  const generateLanguageCountObj = (allLanguageList) => {
-    const notNullLanguageList = allLanguageList.filter(language => language != null);
-    const uniqueLanguageList = [...new Set(notNullLanguageList)]
+    useEffect(() => {
+        if (state.requestState !== requestStates.loading) { return; }
+        fetchReposApi();
+    }, [state.requestState]);
 
-    return uniqueLanguageList.map(item => {
-      return {
-        language: item,
-        count: allLanguageList.filter(language => language === item).length
-      }
-    });
-  };
+    useEffect(() => {
+        dispatch({ type: actionTypes.fetch });
+    }, []);
 
-  const converseCountToPercentage = (count) => {
-    if (count > 10) { return 100; }
-    return count * 10;
+    const generateLanguageCountObj = (allLanguageList) => {
+        const notNullLanguageList = allLanguageList.filter(language => language != null);
+        const uniqueLanguageList = [...new Set(notNullLanguageList)]
+
+        return uniqueLanguageList.map(item => {
+            return {
+                language: item,
+                count: allLanguageList.filter(language => language === item).length
+            }
+        });
+    };
+  const DEFAULT_MAX_PERCENTAGE = 100;
+  const LANGUAGE_COUNT_BASE = 10;
+
+  const convertCountToPercentage = (languageCount) => {
+    if (languageCount > LANGUAGE_COUNT_BASE) { return DEFAULT_MAX_PERCENTAGE; }
+    return languageCount * LANGUAGE_COUNT_BASE;
   };
 
   const sortedLanguageList = () => (
     state.languageList.sort((firstLang, nextLang) =>  nextLang.count - firstLang.count)
   )
-  return [sortedLanguageList, state.requestState, converseCountToPercentage, dispatch];
+  return [sortedLanguageList, state.requestState, convertCountToPercentage, dispatch];
 }
